60d1396040d0a4d8e75952a106c89ab1975d27ed
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-06 22:19:55 +0530

    Update pgm2-refactored.c

--- DSA/pgm2-refactored.c
+++ DSA/pgm2-refactored.c
@@ -79,6 +79,21 @@ void main() {
                break;
     }
   }
-}
+} 
 
-// The code above generates a menu-driven program in C for stack operations including push, pop, palindrome checking, display, and exit. It uses arrays to implement a stack with a maximum size of MAX and includes functions for each operation as requested.
+
+
+
+In the given C program, we have an array-based implementation of a stack with a maximum size defined by MAX. The program begins by defining some variables such as item, ch, top, and status, along with initializing the stack to empty with a top value set to -1 and status set to 0.
+
+The `push` function is implemented to add an element to the stack. If the stack is full (top is equal to MAX-1), it prints an overflow message. Otherwise, the item is added to the stack by incrementing the top index and increasing the status value.
+
+The `pop` function checks if the stack is empty (top is -1) and if so, it prints an underflow message. If the stack is not empty, it returns the popped value after decrementing the top index and decreasing the status value.
+
+In the `palindrome` function, we check if a string is palindromic by comparing each character in the stack with the popped value. If all characters match, we print "Palindrome" as the output. Otherwise, we print "not palindrome".
+
+Lastly, in the `display` function, we check if the stack is empty and if so, we print a message saying that the stack is empty. If the stack is not empty, we display its contents by iterating from top to 0, printing each element using printf with the format specifier "%d".
+
+In the main function, we use a while loop to provide a menu-driven interface for the user. The program continuously displays options for push, pop, palindrome checking, and displaying the stack. The user can input their choice from 1 to 5, and based on the selection, corresponding operations are performed using the functions defined earlier in the program.
+
+Overall, this program allows users to interactively perform various stack-related operations such as pushing and popping elements, checking for palindromic strings, and displaying the status of the stack.
