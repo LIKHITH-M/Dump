b58fde299eae4ad2b54f47c34ab43072f11cb16a
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-08 20:41:19 +0530

    Update pgm5-refactored.c

--- DSA/pgm5-refactored.c
+++ DSA/pgm5-refactored.c
@@ -1,3 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define a structure for a node in the linked list, containing an integer data and a pointer to the next node
+typedef struct node {
+    int data;            // Data field to store the value
+    struct node *link;   // Pointer to the next node
+} NODE;
+
+// Declare pointers for linked lists A, B, C (union), and D (intersection)
+NODE *LLone, *LLtwo, *unionLL, *interLL;
+
+// Function to insert a node at the beginning of the linked list
+NODE* insert(NODE **first, int num)
+{
+    // Allocate memory for the new node
+    NODE* newNode = (NODE*) malloc(sizeof(NODE));
+    newNode->data = num;  // Assign data to the node
+    newNode->link = *first;  // Set the new node to point to the current first node
+    *first = newNode;  // Update the first pointer to point to the new node
+    return *first;  // Return the updated list
+}
+
+// Function to search for an element in the linked list
+int search(NODE *first, int num)
+{
+    // Traverse the linked list
+    while (first != NULL) {
+        // Check if the current node's data matches the search value
+        if (first->data == num) {
+            return 1;  // Return 1 if the value is found
+        }
+        first = first->link;  // Move to the next node
+    }
+    return 0;  // Return 0 if the value is not found
+}
+
+// Function to find the union of two linked lists
+NODE* findunion(NODE *LLone, NODE *LLtwo)
+{
+    unionLL = NULL;  // Initialize the union list as empty
+    NODE *temp = LLone;
+    
+    // Insert all elements of LLone into unionLL
+    while (temp != NULL) {
+        insert(&unionLL, temp->data);  // Insert each element into unionLL
+        temp = temp->link;  // Move to the next node in LLone
+    }
+
+    // Insert elements of LLtwo that are not already in LLone
+    while (LLtwo != NULL) {
+        if (!search(LLone, LLtwo->data)) {  // Check if the element is not in LLone
+            insert(&unionLL, LLtwo->data);  // Insert the element into unionLL
+        }
+        LLtwo = LLtwo->link;  // Move to the next node in LLtwo
+    }
+    return unionLL;  // Return the union linked list
+}
+
+// Function to find the intersection of two linked lists
+NODE* intersection(NODE *LLone, NODE *LLtwo)
+{
+    interLL = NULL;  // Initialize the intersection list as empty
+    
+    // Traverse LLone and check if its elements are present in LLtwo
+    while (LLone != NULL) {
+        if (search(LLtwo, LLone->data)) {  // Check if the element is in LLtwo
+            insert(&interLL, LLone->data);  // Insert the common element into interLL
+        }
+        LLone = LLone->link;  // Move to the next node in LLone
+    }
+    return interLL;  // Return the intersection linked list
+}
+
+// Function to print the elements of a linked list
+void printList(NODE *cur)
+{
+    // Traverse and print each node in the list
+    while (cur != NULL) {
+        printf("-->%d", cur->data);  // Print the data of the current node
+        cur = cur->link;  // Move to the next node
+    }
+}
+
+// Main function to handle user input and perform union and intersection operations
+void main()
+{
+    int i, LLonecount, LLtwocount, temp;
+
+    // Get the number of nodes for the first linked list (LLone)
+    printf("\n Enter number of nodes in first Linked List: ");
+    scanf("%d", &LLonecount);
+    
+    // Get the data for the first linked list (LLone)
+    printf("\n Enter data of first linked list: ");
+    for(i=0; i<LLonecount; i++) {
+        scanf("%d", &temp);  // Read the data value
+        insert(&LLone, temp);  // Insert the data into LLone
+    }
+    printList(LLone);  // Print the elements of LLone
+
+    // Get the number of nodes for the second linked list (LLtwo)
+    printf("\n Enter number of nodes in second Linked List: ");
+    scanf("%d", &LLtwocount);
+
+    // Get the data for the second linked list (LLtwo)
+    printf("\n Enter data of second linked list: ");
+    for(i=0; i<LLtwocount; i++) {
+        scanf("%d", &temp);  // Read the data value
+        insert(&LLtwo, temp);  // Insert the data into LLtwo
+    }
+    printList(LLtwo);  // Print the elements of LLtwo
+
+    // Find the union of LLone and LLtwo
+    findunion(LLone, LLtwo);
+    
+    // Find the intersection of LLone and LLtwo
+    intersection(LLone, LLtwo);
+
+    // Print the union linked list
+    printf("\nUnion Linked List\n");
+    printList(unionLL);
+
+    // Print the intersection linked list
+    printf("\nIntersection Linked List\n");
+    printList(interLL);
+}
 
 
 
