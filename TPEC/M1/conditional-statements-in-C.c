ceb1ae930a03a7c44da6b84f920333211c0ba572
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-13 13:49:29 +0530

    Create conditional-statements-in-C.c

---
+++ TPEC/M1/conditional-statements-in-C.c
@@ -0,0 +1,14 @@
+//erase everything and write below code
+
+    // Write Your Code Here
+    #include<stdio.h>
+
+    int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    printf((n==1)?"one": (n==2)?"two":(n==3)?"three":(n==4)?"four":(n==5)?"five":(n==6)?"six":(n==7)?"seven":(n==8)?"eight":(n==9)?"nine":"Greater than 9");
+
+    return 0;
+}
