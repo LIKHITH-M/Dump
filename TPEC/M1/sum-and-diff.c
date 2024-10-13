59c7bdb6cd2e8f01009419f86d8091ebe5ce16fc
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-13 13:44:43 +0530

    Create sum-and-diff.c

---
+++ TPEC/M1/sum-and-diff.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{   int a,b;
+	scanf("%d %d",&a,&b);
+    int sum = a+b;
+    int diff = a-b;
+    float a1,b1;
+    scanf("%f %f",&a1,&b1);
+    float sum1= a1+b1;
+    //sum1=round(sum1*100)/100; 
+    float diff1 = a1-b1;
+    //diff1=round(diff1*100)/100; 
+    printf("%d %d\n",sum,diff);
+     printf("%.1f %.1f \n",sum1,diff1);
+    
+    
+    return 0;
+}
