cf9cbf271d1aaf75dd0a2ccc241a4b65ec0a882f
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-13 14:17:18 +0530

    Update and rename Correctness and loop variant.txt to Correctness and loop variant.c

--- TPEC/M2/Correctness and loop variant.c
+++ TPEC/M2/Correctness and loop variant.c
@@ -1,6 +1,3 @@
-// Correctness and loop variant
-
-
 #include <stdio.h>
 
 void insertionSort(int N, int arr[]) {
@@ -8,22 +5,25 @@ void insertionSort(int N, int arr[]) {
     for(i = 1; i < N; i++) {
         value = arr[i];
         j = i - 1;
+        
+        // Fix the while loop condition to correctly compare all elements( only for loop iteration values 
         while(j >= 0 && value < arr[j]) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
         arr[j + 1] = value;
     }
-
+    
+    // Print the array only once, after sorting
     for(j = 0; j < N; j++) {
         printf("%d", arr[j]);
-        if(j < N - 1) {
+        if(j < N - 1) { // this if conditiion is the only thing to be added here
             printf(" ");
         }
     }
     printf("\n");
 }
-
+// everything below here is given and is correct
 int main(void) {
     int N;
     scanf("%d", &N);
@@ -37,4 +37,5 @@ int main(void) {
 
     return 0;
 }
+
 
