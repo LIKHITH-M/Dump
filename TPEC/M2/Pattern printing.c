6e8264b3aa67faaea35770bf6687bc4168d6c52f
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-13 14:10:39 +0530

    Update and rename Pattern printing.txt to Pattern printing.c

--- TPEC/M2/Pattern printing.c
+++ TPEC/M2/Pattern printing.c
@@ -1,22 +1,22 @@
-// Pattern printing
-
 #include <stdio.h>
-
+//from here 
 void printPattern(int n) {
     int size = 2 * n - 1;
     
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-          
+            // Find the minimum of the distance to the edges
             int min = i < j ? i : j;
             min = min < size - i ? min : size - i - 1;
             min = min < size - j ? min : size - j - 1;
+            
+            // Print the corresponding value
             printf("%d ", n - min);
         }
         printf("\n");
     }
 }
-
+//till here
 int main() {
     int n;
     scanf("%d", &n);
