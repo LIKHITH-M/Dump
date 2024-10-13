4c63037f784d9a6d141200c956288198e169cea2
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-13 14:45:17 +0530

    Create leetcode-add-two-numbers.c

---
+++ TPEC/M3/leetcode-add-two-numbers.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    int carry = 0;
+    struct ListNode head;
+    struct ListNode* cur = &head;
+    
+    while (l1 || l2 || carry) {
+        int sum = carry;
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        
+        carry = sum / 10;
+        
+        cur->next = malloc(sizeof(struct ListNode));
+        cur = cur->next;
+        cur->val = sum %= 10;
+        cur->next = NULL;
+    }
+    
+    return head.next;
+}
