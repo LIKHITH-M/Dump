de68b5204e95dd6b55cd0441bf3313b01d83c8da
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-22 18:26:25 +0530

    Update Correctness and loop variant.c

--- TPEC/M2/Correctness and loop variant.c
+++ TPEC/M2/Correctness and loop variant.c
@@ -1,3 +1,21 @@
+/* Loop Invariant
+In computer science, you could prove it formally with a loop invariant, where you state that a desired property is maintained in your loop. Such a proof is broken down into the following parts:
+
+    Initialization: It is true (in a limited sense) before the loop runs.
+    Maintenance: If it's true before an iteration of a loop, it remains true before the next iteration.
+    Termination: It will terminate in a useful way once it is finished.
+
+Challenge
+In the InsertionSort code below, there is an error. Can you fix it? Print the array only once, when it is fully sorted.
+
+Input Format
+There will be two lines of input:
+s - the size of the array
+arr - the list of numbers that makes up the array
+
+Output Format
+Output the numbers in order, space-separated on one line.*/
+// Study the whole code unless u dont have enough time.. I dont think they'll give the incorrect code..
 #include <stdio.h>
 
 void insertionSort(int N, int arr[]) {
