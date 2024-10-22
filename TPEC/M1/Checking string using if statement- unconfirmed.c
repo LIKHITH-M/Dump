5b1c17fbc685e58bffa6699e0e79530d57dca6aa
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-10-22 18:11:43 +0530

    Update Checking string using if statement- unconfirmed.c

--- TPEC/M1/Checking string using if statement- unconfirmed.c
+++ TPEC/M1/Checking string using if statement- unconfirmed.c
@@ -1,3 +1,13 @@
+/* Given a sentence S, print each word of the sentence S in a new line.
+
+Input Format
+The first and only line contains a sentence,
+
+Output Format:
+Print each word of the sentence in a new line.
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -8,12 +18,12 @@ char *s;
 s = malloc(1024 * sizeof(char));
 scanf("%[^\n]", s);
 s = realloc(s, strlen(s) + 1);
-//Write your logic frome here
+//From here
 for(int i=0;i<strlen(s);i++){
     if(*(s+i)==' ')
         printf("\n");
     else
         printf("%c",*(s+i));
 }free(s);
-  //till here
+//till here
 return 0;}
